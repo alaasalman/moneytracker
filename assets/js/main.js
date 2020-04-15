1ac5101e98e425d48ddee7a19c2fb690d237a8b4
Alaa Salman <alaa@codedemigod.com>
2020-04-15 19:26:06 +1000

    Added sentry JS error tracking. Not sure if it is working yet, I can't figure out how to generate errors.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import Vue from 'vue';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
 import {
   faHome, faBook, faMoneyBillAlt,
@@ -25,5 +27,9 @@ library.add(
 // will automatically find any <i> tags in the page and replace those with <svg> elements
 dom.watch(); // This will kick of the initial replacement of i to svg tags and configure a MutationObserver
 
+Sentry.init({
+  dsn: 'https://f5ea7a819860428ba6dddbe5c1e47044@o123413.ingest.sentry.io/5199529',
+  integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
+});
 
 
